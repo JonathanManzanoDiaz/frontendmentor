c67b9174b337bf068a0e6d50c768eae1279430f0
JonathanManzanoDiaz <43423216+JonathanManzanoDiaz@users.noreply.github.com>
2022-12-10 14:41:44 +0100

    Fix hamburger menu on blogr landing page

--- ALL-CHALLENGES/026-blogr-landing-page/js/app.js
+++ ALL-CHALLENGES/026-blogr-landing-page/js/app.js
@@ -1,19 +1,10 @@
-const hamb = document.querySelector(".hamburger");
-const menu = document.querySelector(".menu");
-const headingLi = document.querySelectorAll(".headingLi");
-const menuBox = document.querySelectorAll(".menuBox2");
-hamb.addEventListener("click", () => {
-  if (menu.classList.contains("show")) {
-    menu.classList.remove("show");
-    hamb.src = "../images/icon-close.svg";
-  } else {
-    menu.classList.add("show");
-    hamb.src = "../images/icon-hamburger.svg";
-  }
-});
-
-headingLi.forEach((item) => {
-  item.addEventListener("click", () => {
-    item.childNodes[3].classList.toggle("show");
+// A $( document ).ready() block.
+$(document).ready(function () {
+  const close = document.querySelector(".close");
+  const menu = document.querySelector(".menu");
+  const headingLi = document.querySelectorAll(".headingLi");
+  const menuBox = document.querySelectorAll(".menuBox2");
+  $("#hamb").click(function () {
+    $("#menu").toggle("show");
   });
 });
